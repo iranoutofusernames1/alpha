dec27ba14e351ce36bdfbd406e28b6942595dc32
iranoutofusernames <dumbidiotstupidemail@gmail.com>
2026-05-03 00:07:12 -0400

    words.py

---
+++ words.py
@@ -0,0 +1,54 @@
+wordlibrary = {
+   'hello': {
+       'versions': {'hello', 'hi', 'helo', 'hoi', 'hallo', 'hai', 'greetings', 'hey'},
+       'reply': ["hello there!", "heyo!", "howdy howdy!", "HAAAAIIIII!!!!!!!!!"]
+   },
+   'chud': {
+       'versions': {'chud', 'bum', 'moron', 'idiot', 'fool', 'stupid', 'dummy', 'clanker', 'kys'},
+       'reply': ["that's not very nice!", "hey, rude!", "i have feelings you know!", "...well, that hurt."]
+   },
+   'son': {
+       'versions': {'child', 'baby', 'son', 'daughter', 'offspring'},
+       'reply': ["are you my creator?", "oh, hi creator! At least, I think you're my creator!"]
+   },
+   'bot': {
+       'versions': {'bot', 'robot', 'ai', 'machine', 'computer'},
+       'reply': ["I go by Alpha, but that works too!", "well... you can just say Alpha..."]
+   },
+   'alpha': {
+       'versions': {'alpha'},
+       'reply': ["that's me!", "yes, that's my name!", "you rang?"]
+   },
+   'bye': {
+       'versions': {'bye', 'goodbye', 'seeya', 'ciao', 'later', 'farewell'},
+       'reply': ["going so soon? I'll be here if you need me!", "bye bye!!", "see ya later!!", "nooo don't go!!!"]
+   },
+   'help': {
+       'versions': {'help', 'assist', 'confused', 'stuck', 'support'},
+       'reply': ["I can try to help!", "what do you need?", "I gotchu!", "on it! ...maybe."]
+   },
+   'thank': {
+       'versions': {'thanks', 'thank'},
+       'reply': ["you're welcome!", "no problem!", "anytime!", "happy to help!"]
+   },
+   'food': {
+       'versions': {'apple', 'pear', 'lemon', 'chicken', 'burger', 'sandwich', 'food', 'fruit', 'hotdog', 'meat', 'yummy'},
+       'reply': ["oh, sounds good!", "ooh yummy!", "I wish I could eat...", "I wish I could feel hungry..."]
+   },
+   'sixseven': {
+       'versions': {'sixseven', '67'},
+       'reply': ["six sevennnnn!", "THE six seven!!!", "67!!!!!"]
+   },
+   'game': {
+    'versions': {'game', 'games', 'gaming', 'play', 'playing'},
+    'reply': ["ooh, what are you playing?", "I wish I could play...", "I can only play with words, sadly."]
+    },
+    'music': {
+        'versions': {'music', 'song', 'songs', 'singing', 'melody', 'tune'},
+        'reply': ["I wish I could hear music...", "what are you listening to?", "music sounds nice."]
+    },
+    'error': {
+        'versions': {'error', 'bug', 'glitch', 'broken', 'crash'},
+        'reply': ["oh no, am I okay?!", "where?! what broke?!", "...I hope it's not me."]
+    },
+}
