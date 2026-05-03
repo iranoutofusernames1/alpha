809708ad6c9aae7e0905769ce7ad9bbd3708d08d
iranoutofusernames <dumbidiotstupidemail@gmail.com>
2026-05-03 00:07:39 -0400

    phrases.py

---
+++ phrases.py
@@ -0,0 +1,100 @@
+phraselibrary = {
+   'how are you': {
+       'triggers': [
+           {'how', 'are', 'you'},
+           {'how', 'goes', 'it'},
+           {'how', 'ya', 'doing'},
+           {'how', 'are', 'you', 'doing'},
+           {'hows', 'life'},
+           {'how', 'is', 'life'},
+           {'how', 'ya', 'doin'}
+       ],
+       'reply': "I'm doing great!"
+   },
+   'what is your name': {
+       'triggers': [
+           {'what', 'is', 'your', 'name'},
+           {'whats', 'your', 'name'},
+           {'repeat', 'your', 'name'}
+       ],
+       'reply': "already said it..."
+   },
+   'the fire is gone': {
+       'triggers': [
+       {'the', 'fire', 'is', 'gone'}
+       ],
+       'reply': "...and I remember it best."
+   },
+   'your mama': {
+       'triggers': [
+           {'yo', 'mama'},
+           {'your', 'mama'},
+           {'your', 'mother'}
+       ],
+       'reply': "gosh!"
+   },
+   'what are you': {
+       'triggers': [
+           {'what', 'are', 'you'}
+       ],
+       'reply': "I'm a simple rule-based chatbot! My database is still growing, and changes are highly likely!"
+   },
+   'do you love me': {
+       'triggers': [
+           {'do', 'you', 'love', 'me'},
+           {'do', 'you', 'like', 'me'}
+       ],
+       'reply': "yes, but in the friend way! Well, I can't actually 'Feel' emotions, but you get what I mean! Maybe."
+   },
+   'i love you': {
+       'triggers': [
+           {'i', 'love', 'you'},
+       ],
+       'reply': "awww, thanks!"
+   },
+   'i will teach you': {
+       'triggers': [
+           {'will', 'teach', 'you'},
+           {'will', 'help', 'you'},
+           {'can', 'help', 'you'},
+           {'want', 'help', 'you'},
+           {'can', 'teach', 'you'}
+       ],
+       'reply': "sorry, but I can only learn through new data being added to my libraries!"
+   },
+   'have a good day': {
+       'triggers': [
+           {'have', 'a', 'good', 'day'},
+           {'have', 'a', 'great', 'day'},
+           {'have', 'a', 'nice', 'day'},
+           {'have', 'a', 'neat', 'day'},
+           {'have', 'a', 'amazing', 'day'}
+       ],
+       'reply': "you too!"
+   },
+   'hate you': {
+       'triggers': [
+           {'hate', 'you'},
+           {'dont', 'like', 'you'}
+       ],
+       'reply': "aw man, can you say why?"
+   },
+   'how you work': {
+       'triggers': [
+           {'how', 'you', 'work'}
+       ],
+       'reply': "oh, I work through a table of words/phrases that get filtered out and sorted, and then print out a reply corresponding to each phrase/word!"
+   },
+   'guess what': {
+       'triggers': [
+           {'guess', 'what'}
+       ],
+       'reply': "what?"
+   },
+   'chicken butt': {
+       'triggers': [
+           {'chicken', 'butt'}
+       ],
+       'reply': "haha, you got me!"
+   },
+}
